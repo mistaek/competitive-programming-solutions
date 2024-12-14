941147ad9db76d36921d5829d9b5f32cee0240d3
ray <61148858+mistaek@users.noreply.github.com>
2024-12-14 14:44:26 -0500

    fix solution

--- cf/1009E.cpp
+++ cf/1009E.cpp
@@ -1,5 +1,6 @@
 /*
 1009E
+AC, oddly slow
 */
 
 #include <iostream>
@@ -8,9 +9,9 @@
 using namespace std; 
 
 long long MOD = 998244353;
-long long pow(long long a, long long exp){
+long long binexp(long long a, long long exp){
     long long ans = 1, pow = a; 
-    for(int i = 0; i < 60; ++i){
+    for(int i = 0; i < 21; ++i){
         if(exp & (1LL << i)) ans *= pow; 
         ans %= MOD; 
         pow *= pow; 
@@ -24,15 +25,15 @@ int main(){
     cin.tie(0); 
 
     int n; cin >> n; 
-    vector<int> a(n+1); 
+    long long a;
     long long ans = 0;
     for(int i = 1; i <= n; ++i){
-        cin >> a[i];
-        long long s = pow(2, n-i);
+        cin >> a;
+        long long s = binexp(2, n-i);
         s %= MOD; 
-        if(n-i-1 >= 0) s += (n-i) *pow(2, n-i-1); 
+        if(n-i-1 >= 0) s += (n-i) *binexp(2, n-i-1); 
         s %= MOD; 
-        s *= a[i]; 
+        s *= a;
         s %= MOD; 
         ans += s; 
         ans %= MOD; 
